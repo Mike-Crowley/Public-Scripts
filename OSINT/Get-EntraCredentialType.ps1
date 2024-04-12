18e368874402de1868f0595c1780c2120bad0dd4
Mike Crowley <mike@mikecrowley.us>
2024-04-12 16:24:54 -0400

    Create Get-EntraCredentialType.ps1
    Moved from https://gist.github.com/Mike-Crowley/b405b4ed46f55bc8039c81423ed78355

---
+++ OSINT/Get-EntraCredentialType.ps1
@@ -0,0 +1,35 @@
+<#
+.SYNOPSIS
+    Misc OSINT
+
+.EXAMPLE
+
+    Get-EntraCredentialType -Upn user1@domain.com
+
+.LINK
+
+    https://mikecrowley.us
+#>
+Function Get-EntraCredentialType {
+    param (
+        [parameter(Mandatory = $true)][string]
+        $Upn
+    )
+    $Body = @{
+        username            = $Upn
+        isOtherIdpSupported = $true
+    }
+    $Body = $Body | ConvertTo-Json -Compress
+    $Response = Invoke-RestMethod -Method Post -Uri "https://login.microsoftonline.com/common/GetCredentialType" -Body $Body
+    
+    [pscustomobject]@{
+        Username              = $Response.Username
+        PrefCredential        = $Response.Credentials.PrefCredential
+        DomainFound           = $Response.IfExistsResult -eq 0
+        FederatedDomain       = $null -ne $Response.Credentials.FederationRedirectUrl
+        FederationRedirectUrl = $Response.Credentials.FederationRedirectUrl
+        DesktopSsoEnabled     = $Response.EstsProperties.DesktopSsoEnabled
+        UserTenantBranding    = $Response.EstsProperties.UserTenantBranding
+    }
+    
+}
